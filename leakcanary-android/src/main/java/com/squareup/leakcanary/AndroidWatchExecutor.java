3cf8a2ae1b3d4ace42d81f3632c6262e733903f6
kailaisii <541018378@qq.com>
2020-05-24 23:39:06 +0800

    完善输出的文档中所使用的注释信息

--- leakcanary-android/src/main/java/com/squareup/leakcanary/AndroidWatchExecutor.java
+++ leakcanary-android/src/main/java/com/squareup/leakcanary/AndroidWatchExecutor.java
@@ -34,11 +34,13 @@ public final class AndroidWatchExecutor implements WatchExecutor {
   static final String LEAK_CANARY_THREAD_NAME = "LeakCanary-Heap-Dump";
   private final Handler mainHandler;
   private final Handler backgroundHandler;
+  //默认5秒
   private final long initialDelayMillis;
   private final long maxBackoffFactor;
 
   public AndroidWatchExecutor(long initialDelayMillis) {
     mainHandler = new Handler(Looper.getMainLooper());
+    //HandlerThread是一种拥有Looper的线程
     HandlerThread handlerThread = new HandlerThread(LEAK_CANARY_THREAD_NAME);
     handlerThread.start();
     backgroundHandler = new Handler(handlerThread.getLooper());
@@ -47,14 +49,17 @@ public final class AndroidWatchExecutor implements WatchExecutor {
   }
 
   @Override public void execute(@NonNull Retryable retryable) {
+    //如果当前线程是主线程，则直接执行waitForIdl
     if (Looper.getMainLooper().getThread() == Thread.currentThread()) {
       waitForIdle(retryable, 0);
     } else {
+      //如果不是主线程，则通过Handler机制，将waitForIdle放入到主线程去执行
       postWaitForIdle(retryable, 0);
     }
   }
 
   private void postWaitForIdle(final Retryable retryable, final int failedAttempts) {
+    //通过Handler机制，将waitForIdle发送到主线程执行
     mainHandler.post(new Runnable() {
       @Override public void run() {
         waitForIdle(retryable, failedAttempts);
@@ -64,6 +69,8 @@ public final class AndroidWatchExecutor implements WatchExecutor {
 
   private void waitForIdle(final Retryable retryable, final int failedAttempts) {
     // This needs to be called from the main thread.
+    //当messagequeue闲置时，增加一个处理。这种方法主要是为了提升性能，不会影响我们正常的应用流畅度
+    //这个方法会在主线程执行，所以postToBackgroundWithDelay会在主线程执行
     Looper.myQueue().addIdleHandler(new MessageQueue.IdleHandler() {
       @Override public boolean queueIdle() {
         postToBackgroundWithDelay(retryable, failedAttempts);
@@ -73,8 +80,12 @@ public final class AndroidWatchExecutor implements WatchExecutor {
   }
 
   private void postToBackgroundWithDelay(final Retryable retryable, final int failedAttempts) {
+    //计算补偿因子。如果返回了重试的话，这个failedAttempts回增加，会使得方法的执行时间延迟时间增加。
+    //比如说第一次，演示5秒执行，但是执行结果为RETRY，那么下一次就是延迟10秒来执行了
     long exponentialBackoffFactor = (long) Math.min(Math.pow(2, failedAttempts), maxBackoffFactor);
+    //计算延迟时间
     long delayMillis = initialDelayMillis * exponentialBackoffFactor;
+    //backgroundHandler会将run方法中的代码放在一个新的线程中去执行。
     backgroundHandler.postDelayed(new Runnable() {
       @Override public void run() {
         Retryable.Result result = retryable.run();
